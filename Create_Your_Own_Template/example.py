39856047afee726ce34dae521f8ad173774eaba9
Sven <67136317+Sven-Bo@users.noreply.github.com>
2022-11-01 08:53:33 +0100

    Implement more robust path settings & housekeeping 🧹

--- Create_Your_Own_Template/example.py
+++ Create_Your_Own_Template/example.py
@@ -1,13 +1,19 @@
-import os, sys  # Standard Python Libraries
+from pathlib import Path  # core library
+
+from docx.shared import Cm, Emu, Inches, Mm  # pip install python-docx
 from docxtpl import DocxTemplate, InlineImage  # pip install docxtpl
-from docx.shared import Cm, Inches, Mm, Emu  # pip install python-docx
 
-# Change path to current working directory
-os.chdir(sys.path[0])
+# Path settings
+current_dir = Path(__file__).parent
+template_path = current_dir / "Template.docx"
+output_path = current_dir / "Template_Rendered.docx"
+img_placeholder1_path = current_dir / "Placeholders" / "Placeholder_1.png"
+img_placeholder2_path = current_dir / "Placeholders" / "Placeholder_2.png"
 
-doc = DocxTemplate("Template.docx")
-placeholder_1 = InlineImage(doc, "Placeholders/Placeholder_1.png", Cm(5))
-placeholder_2 = InlineImage(doc, "Placeholders/Placeholder_2.png", Cm(5))
+
+doc = DocxTemplate(template_path)
+placeholder_1 = InlineImage(doc, str(img_placeholder1_path), Cm(5))
+placeholder_2 = InlineImage(doc, str(img_placeholder2_path), Cm(5))
 context = {
     "name": "Sven",
     "placeholder_1": placeholder_1,
@@ -15,4 +21,4 @@ context = {
 }
 
 doc.render(context)
-doc.save("Template_Rendered.docx")
+doc.save(output_path)
--- word_automation.py
+++ word_automation.py
@@ -1,32 +1,30 @@
-import os, sys  # Standard Python Libraries
+from pathlib import Path  # core library
+
+import matplotlib.pyplot as plt  # pip install matplotlib
+import pandas as pd  # pip install pandas
+import win32com.client as win32  # pip install pywin32
 import xlwings as xw  # pip install xlwings
 from docxtpl import DocxTemplate  # pip install docxtpl
-import pandas as pd  # pip install pandas
-import matplotlib.pyplot as plt  # pip install matplotlib
-import win32com.client as win32  # pip install pywin32
 
 # -- Documentation:
 # python-docx-template: https://docxtpl.readthedocs.io/en/latest/
 
-# Change path to current working directory
-os.chdir(sys.path[0])
 
-
-def create_barchart(df, barchart_name):
+def create_barchart(df, barchart_output):
     """Group DataFrame by sub-category, plot barchart, save plot as PNG"""
     top_products = df.groupby(by=df["Sub-Category"]).sum()[["Sales"]]
     top_products = top_products.sort_values(by="Sales")
     plt.rcParams["figure.dpi"] = 300
     plot = top_products.plot(kind="barh")
     fig = plot.get_figure()
-    fig.savefig(f"{barchart_name}.png", bbox_inches="tight")
+    fig.savefig(barchart_output, bbox_inches="tight")
     return None
 
 
 def convert_to_pdf(doc):
     """Convert given word document to pdf"""
     word = win32.DispatchEx("Word.Application")
-    new_name = doc.replace(".docx", r".pdf")
+    new_name = doc.replace(".docx", ".pdf")
     worddoc = word.Documents.Open(doc)
     worddoc.SaveAs(new_name, FileFormat=17)
     worddoc.Close()
@@ -34,27 +32,33 @@ def convert_to_pdf(doc):
 
 
 def main():
+    # Path settings
+    current_dir = Path(__file__).parent
+    template_path = current_dir / "Sales_Report_TEMPLATE.docx"
+
+    # Conection to Excel
     wb = xw.Book.caller()
     sht_panel = wb.sheets["PANEL"]
     sht_sales = wb.sheets["Sales"]
-    doc = DocxTemplate("Sales_Report_TEMPLATE.docx")
-    # -- Get values from Excel
     context = sht_panel.range("A2").options(dict, expand="table", numbers=int).value
     df = sht_sales.range("A1").options(pd.DataFrame, index=False, expand="table").value
 
+    # Initialize template
+    doc = DocxTemplate(str(template_path))
+
     # -- Create Barchart & Replace Placeholder
     barchart_name = "sales_by_subcategory"
-    create_barchart(df, barchart_name)
-    doc.replace_pic("Placeholder_1.png", f"{barchart_name}.png")
+    barchart_output = current_dir / f"{barchart_name}.png"
+    create_barchart(df, barchart_output)
+    doc.replace_pic("Placeholder_1.png", barchart_output)
 
     # -- Render & Save Word Document
-    output_name = f'Sales_Report_{context["month"]}.docx'
+    output_name = current_dir / f'Sales_Report_{context["month"]}.docx'
     doc.render(context)
     doc.save(output_name)
 
     # -- Convert to PDF [OPTIONAL]
-    path_to_word_document = os.path.join(os.getcwd(), output_name)
-    convert_to_pdf(path_to_word_document)
+    convert_to_pdf(str(output_name))
 
     # -- Show Message Box [OPTIONAL]
     show_msgbox = wb.macro("Module1.ShowMsgBox")
